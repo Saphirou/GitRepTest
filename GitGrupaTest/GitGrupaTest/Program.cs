0d731ae0691a703e57a21d7861cecc5589105d8d
Lukasz AKA GitMaster ;) <dagsdg>
2015-07-03 21:39:12 +0200

    Wymuszam konflikt Merge'a

--- GitGrupaTest/GitGrupaTest/Program.cs
+++ GitGrupaTest/GitGrupaTest/Program.cs
@@ -10,7 +10,9 @@ namespace GitGrupaTest
     {
         static void Main(string[] args)
         {
+            // Ta zmiana jest przed Mateusza zmianą, więc powinień zgłosić konflikt i chcieć Merge'ować. Chyba...
             Console.WriteLine("Łukasz,...");
+            //Coś mi Twojego komita w gicie nie wyswietla Mateusz :/ Widzę na stronie, ale nie w konsoli. Komituje żeby sprawdzić czy Merge'a będzie chciał robić.
         }
     }
 }
